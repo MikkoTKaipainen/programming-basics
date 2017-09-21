937f9f61fdc9bc12e10dc6f785d3a4d60936380f
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2017-09-21 15:44:32 +0300

    Initial programming

---
+++ conditional-statements/condional-statement/condional-statement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace condional_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla.");
+            Console.Write("Syota luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int.TryParse(userInput, out int evaluatedNumber);
+            if (evaluatedNumber < 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on negatiivininen");
+            }
+            else if(evaluatedNumber > 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on positiivinen");
+            }
+            else
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on nolla");
+            }
+            Console.WriteLine($"Syotit arvon {userInput}");
+            Console.ReadKey();
+        }
+    }
+}
