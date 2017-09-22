c9d06408393be5cc785d7007df17f87876fb52bb
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2017-09-22 15:02:06 +0300

    added lines

---
+++ conditional-statements/condional-statement/task-3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa syotetyn luvun ja kertoo parillinen tai pariton.");
+            Console.Write("Syota luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+            bool isNumber = int.TryParse(userInput, out int evaluatedNumber);
+                string isEven;
+                string isPositive;
+            
+                if(evaluatedNumber % 2 == 0)
+                {
+                    isEven = "Parillinen";
+                }
+                else
+                {
+                    isEven = "Pariton";
+                }
+
+
+                if(evaluatedNumber > 0)
+                {
+                    isPositive = "Positiivinen";
+                }
+                else if(evaluatedNumber < 0)
+                {
+                    isPositive = "Negatiivinen";
+                }
+
+                else
+                {
+                isPositive = "Nolla";
+                }
+
+                Console.WriteLine($"Luku on {isEven} ja {isPositive}");
+                Console.ReadKey();
+        }
+    }
+}
