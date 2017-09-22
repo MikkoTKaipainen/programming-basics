82b2018ff80b1dfb7d5df9bef5c0fbe557ef942d
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2017-09-22 13:01:27 +0300

    added do-while lines

--- conditional-statements/condional-statement/condional-statement/Program.cs
+++ conditional-statements/condional-statement/condional-statement/Program.cs
@@ -7,24 +7,33 @@ namespace condional_statement
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla.");
-            Console.Write("Syota luku: ");
-            string userInput;
-            userInput = Console.ReadLine();
+            bool isNumber;
+            do
+            {
 
-            int.TryParse(userInput, out int evaluatedNumber);
-            if (evaluatedNumber < 0)
-            {
-                Console.WriteLine($"Numero {evaluatedNumber} on negatiivininen");
-            }
-            else if(evaluatedNumber > 0)
-            {
-                Console.WriteLine($"Numero {evaluatedNumber} on positiivinen");
-            }
-            else
-            {
-                Console.WriteLine($"Numero {evaluatedNumber} on nolla");
-            }
-            Console.WriteLine($"Syotit arvon {userInput}");
+                Console.Write("Syota luku: ");
+                string userInput;
+                userInput = Console.ReadLine();
+
+                isNumber = int.TryParse(userInput, out int evaluatedNumber);
+
+                if (isNumber == true)
+
+                if (evaluatedNumber < 0)
+                {
+                    Console.WriteLine($"Numero {evaluatedNumber} on negatiivininen");
+                }
+                else if (evaluatedNumber > 0)
+                {
+                    Console.WriteLine($"Numero {evaluatedNumber} on positiivinen");
+                }
+                else
+                {
+                    Console.WriteLine($"Numero {evaluatedNumber} on nolla");
+                }
+                Console.WriteLine($"Syotit arvon {userInput}");
+                
+            } while (isNumber == false);
             Console.ReadKey();
         }
     }
