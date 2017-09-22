f5821105080ce4a560588a8d83210aec6d359540
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2017-09-22 15:26:13 +0300

    tasks-1-2-3-complete task-4 in progress

---
+++ conditional-statements/condional-statement/task-4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syota kolme lukua ja ohjelma tulostaa syotetyt luvut ja tulostaa ne suuruusjarjestykseen.");
+            Console.Write("Syota luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int numero1 = int.Parse(userInput);
+            Console.WriteLine($"Syota ensimmainen luku: ");
+
+            int numero2 = int.Parse(userInput);
+            Console.WriteLine($"Syota toinen luku: ");
+
+            int numero3 = int.Parse(userInput);
+            Console.WriteLine($"Syota kolmas luku: ");
+
+            if (numero1 < numero2 < numero3);
+            {
+            else 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
