627ffe420a6d4ab3c50fb0820ed9cb797936b193
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2017-09-27 18:49:32 +0300

    completed if else

--- conditional-statements/condional-statement/task-4/Program.cs
+++ conditional-statements/condional-statement/task-4/Program.cs
@@ -7,58 +7,58 @@ namespace task_4
         static void Main(string[] args)
         {
             Console.WriteLine("Syota kolme lukua ja ohjelma tulostaa syotetyt luvut ja tulostaa ne suuruusjarjestykseen.");
-            Console.Write("Syota luku: ");
 
-            Console.WriteLine($"Syota ensimmainen luku: ");
-            string numero1 = Console.ReadLine();
-            int numx = int.Parse(numero1);
+                Console.WriteLine($"Syota ensimmainen luku: ");
+                string numero1 = Console.ReadLine();
+                int numx = int.Parse(numero1);
 
-            Console.WriteLine($"Syota toinen luku: ");
-            string numero2 = Console.ReadLine();
-            int numy = int.Parse(numero2);
+                Console.WriteLine($"Syota toinen luku: ");
+                string numero2 = Console.ReadLine();
+                int numy = int.Parse(numero2);
 
-            Console.WriteLine($"Syota kolmas luku: ");
-            string numero3 = Console.ReadLine();
-            int numz = int.Parse(numero3);
+                Console.WriteLine($"Syota kolmas luku: ");
+                string numero3 = Console.ReadLine();
+                int numz = int.Parse(numero3);
 
-            if(numx < numy)
-            {
-            if (numx < numz)
+                if(numx < numy)
                 {
-                if (numy < numz)
-                {
-                        Console.WriteLine($"Järjestys on seuraava: {numx}, {numy}, {numz}");
+                if (numx < numz)
+                    {
+                    if (numy < numz)
+                    {
+                            Console.WriteLine($"Jarjestys on seuraava: {numx}, {numy}, {numz}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Jarjestys on seuraava: {numx}, {numz}, {numy}");
+                        }
+                    
                     }
                     else
                     {
-                        Console.WriteLine($"Järjestys on seuraava: {numx}, {numz}, {numy}");
+                        Console.WriteLine($"Jarjestys on seuraava: {numz}, {numx}, {numy}");
                     }
-                    
                 }
                 else
                 {
-                    Console.WriteLine($"Järjestys on seuraava: {numz}, {numx}, {numy}");
-                }
-            }
-            else
-            {
                 if (numx < numz)
                 {
                     if (numy < numz)
                     {
-                        Console.WriteLine($"Järjestys on seuraava: {numx}, {numy}, {numz}");
+                        Console.WriteLine($"Jarjestys on seuraava: {numy}, {numz}, {numx}");
                     }
                     else
                     {
-                        Console.WriteLine($"Järjestys on seuraava: {numx}, {numz}, {numy}");
+                        Console.WriteLine($"Jarjestys on seuraava: {numz}, {numy}, {numx}");
                     }
 
                 }
                 else
                 {
-                    Console.WriteLine($"Järjestys on seuraava: {numz}, {numx}, {numy}");
+                    Console.WriteLine($"Jajestys on seuraava: {numy}, {numx}, {numz}");
                 }
-            }
+
+                }
             Console.ReadKey();
         }
     }
