d2a589d7f8886bffc4754cb837617ce4d77c4786
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2017-10-06 14:21:26 +0300

    updated 2.1

--- conditional-statements/conditional-statement2/conditional-statement/task 2.1.1/Program.cs
+++ conditional-statements/conditional-statement2/conditional-statement/task 2.1.1/Program.cs
@@ -15,37 +15,31 @@ namespace task_2._1._1
             float discount = 0;
             bool discounted = false;
             
-            
             //Ikä
             Console.WriteLine($"Kerro ikasi: ");
             int age = int.Parse(Console.ReadLine());
 
-            if (age < 7)
+            if (age <= 7)
             {
                 discount = 100;
                 discounted = true;
             }
-            else if (age > 7 && age < 15)
+            else if (age >= 7 && age <= 15)
             {
                 discount = 50;
                 discounted = true;
             }
-            else if (age < 65)
+            else if (age >= 65)
             {
                 discount = 50;
                 discounted = true;
-            }
-            else
-            {
-                discount = 0;
-                discounted = false;
             }
             
             //Varusmies
             Console.WriteLine($"Oletko varusmees?: k/e ");
             String conscriptAnswer = Console.ReadLine().ToUpper();
 
-            if (conscriptAnswer == "Y" && discounted == false)
+            if (conscriptAnswer == "K" && discounted == false)
             {
                 discount = 50;
             }
@@ -55,36 +49,30 @@ namespace task_2._1._1
                 Console.WriteLine($"Oletko Mtk jasen: k/e?");
                 String mtkAnswer = Console.ReadLine().ToUpper();
 
-                if (mtkAnswer == "Y" && discounted == false)
+                //Opiskelija
+                Console.WriteLine($"Oletko opiskelija?: k/e");
+                String studentAnswer = Console.ReadLine().ToUpper();
+
+                if (mtkAnswer == "K" && discounted == false)
                 {
                     discount = 15;
                 }
                 else
                 {
-                    //Opiskelija
-                    Console.WriteLine($"Oletko opiskelija?: k/e");
-                    String studentAnswer = Console.ReadLine().ToUpper();
-
-                    if (studentAnswer == "Y" && discounted == false)
+                    if (studentAnswer == "K" && discounted == false)
                     {
                         discount = 45;
                     }
-                    else if (studentAnswer == "Y" && mtkAnswer == "Y" && discounted == false)
-                    {
-                        discount = 60;
-                    }
+                else if (studentAnswer == "K" && mtkAnswer == "K" && discounted == false)
+                {
+                    discount = 60;
                 }
-
+                }
 
             }
             //Tulostaa lipun hinnan
-            float hinta = price *discount / 100;
+            float hinta = price - (price * (discount / 100));
             Console.WriteLine($"Lipun hintasi on: {hinta}");
-
-
-
-
-
             Console.ReadLine();
         }
 
