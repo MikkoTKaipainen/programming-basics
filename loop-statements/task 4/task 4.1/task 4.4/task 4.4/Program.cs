64e4e02dc7be166ebb06298659a0993d9d3a38b6
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2017-10-23 09:16:07 +0300

    added loops

---
+++ loop-statements/task 4/task 4.1/task 4.4/task 4.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa kaksikymmenta klaavan ja kruunan heittoa ja tulostaa kuinka monta klaavaa ja kruunaa tulee.");
+            Console.WriteLine("Anna luku, kuinka monta kertaa kolikkoa heitetaan: ");
+            string userInput;
+            userInput = Console.ReadLine();
+            Random rnd = new Random();
+            int heads = 1;
+            int tails = 2;
+
+            
+            
+            Console.Write($"Klaavoja:{tails}, Kruunia:{heads} ");
+            Console.ReadKey();
+        }
+    }
+}
