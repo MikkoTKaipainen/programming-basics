cd8ca3d103b310886db52d5c90f5ab9ff15213df
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2017-10-24 12:46:07 +0300

    adding some shit

--- functions/task 5.2/Program.cs
+++ functions/task 5.2/Program.cs
@@ -8,8 +8,39 @@ namespace task_5._2
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Funktio palauttaa kaksi annettua arvoa ja kertoo kumpi on pienempi");
+            //Muutetaan stringit intiksi
+            int num1 = 0;
+            int num2 = 0;
+            bool userInput = int.TryParse(Console.ReadLine(), out num1);
+            bool userInput2 = int.TryParse(Console.ReadLine(), out num2);
 
+            if (userInput == true && userInput2 == true)
+            {
+                Console.WriteLine($"{minimum(num1, num2)}");
+            }
+            else
+            {
+                Console.WriteLine("Vain numerot ovat hyväksyttäviä");
+            }
+            Console.ReadKey();
             
+        }
+        //Funktio joka hakee luvut ja kertoo järjestyksen
+        static string minimum(int num1, int num2)
+        {
+            string answer;
+
+            if(num1 < num2)
+            {
+                answer = $"Luku {num1} on pinempi kuin {num2}";
+            }
+
+            else
+            {
+                answer = $"Luku {num2} on pienempi kuin {num1}";
+            }
+
+            return answer;
         }
     }
 }
