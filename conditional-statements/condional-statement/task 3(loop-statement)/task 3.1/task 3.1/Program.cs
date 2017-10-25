9a1feb5623e719c790fecf3841dd79b0a33b26e9
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2017-10-25 12:47:56 +0300

    added

--- conditional-statements/condional-statement/task 3(loop-statement)/task 3.1/task 3.1/Program.cs
+++ conditional-statements/condional-statement/task 3(loop-statement)/task 3.1/task 3.1/Program.cs
@@ -6,30 +6,31 @@ namespace task_3._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjelma laskee kertoman. Anna luku, jonka kertoma laskee");
+            Console.WriteLine("Ohjelma laskee kertoman. Anna luku, jonka kertoma lasketaan:");
             string userInput = Console.ReadLine();
+
             int number = int.Parse(userInput);
-            //int.TryParse(userInput, out int number);
+
             int i = 0;
             int f = 1;
+
             if (number < 0)
             {
                 Console.WriteLine("Arvo on virheellinen");
                 Console.ReadLine();
                 return;
             }
+
             do
             {
+
                 i = i + 1;
                 f = f * i;
-            }
-            while (i < number);
-            for (int j = 1; j < number; j++)
-            {
-                f = f * j;
-                Console.WriteLine($"{j}!={f}"); //5! = 120
-            }
-            Console.WriteLine($"Syotit {number}\nvastaus {f}"); //5! = 120
+
+            } while (i < number);
+
+
+            Console.WriteLine($"Syotit: {number} \nVastaus: {f} ");
             Console.ReadKey();
         }
     }
--- functions/task 5.4/Program.cs
+++ functions/task 5.4/Program.cs
@@ -6,7 +6,50 @@ namespace task_5._4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Funktio palauttaa kaksi annettua arvoa ja kertoo kumpi on pienempi");
+            //Muutetaan stringit intiksi
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            int num4 = 0;
+            int num5 = 0;
+            int num6 = 0;
+            int num7 = 0;
+            int num8 = 0;
+            int num9 = 0;
+            int num10 = 0;
+
+            bool userInput = int.TryParse(Console.ReadLine(), out num1);
+            bool userInput2 = int.TryParse(Console.ReadLine(), out num2);
+
+            if (userInput == true && userInput2 == true)
+            {
+                Console.WriteLine($"{minimum(num1, num2)}");
+            }
+            else
+            {
+                Console.WriteLine("Vain numerot ovat hyväksyttäviä");
+            }
+            Console.ReadKey();
+
+        }
+        //Funktio joka hakee luvut ja kertoo järjestyksen
+        static string minimum(int num1, int num2)
+        {
+            string answer;
+
+            if (num1 < num2)
+            {
+                answer = $"Luku {num1} on pinempi kuin {num2}";
+            }
+
+            else
+            {
+                answer = $"Luku {num2} on pienempi kuin {num1}";
+            }
+
+            return answer;
         }
     }
 }
