5b6ea8492441078191b7bbaafe115f56f991d03e
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2017-11-17 15:16:45 +0200

    adddeed

--- arrays/task 3/task 3.2/task 3.2/Program.cs
+++ arrays/task 3/task 3.2/task 3.2/Program.cs
@@ -4,37 +4,45 @@ namespace task_3
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Kyseessä on taulukko joka arpoo 10 lukua väliltä 0 - 100");
             Random rnd = new Random();
-            string[ , ] numbers = new string[0,100];
+            int[ , ] numbers = new int[10,20];
 
-            Console.WriteLine("[X , Y] = Arvo");
+            Console.WriteLine("[ X ,  Y] = Arvo");
 
             for (int i = 0; i < 10; i++)
             {
-                int j = i + 10;
-                numbers[i,i] = getNum(rnd.Next(1,101));
-                Console.WriteLine($"[{i} , {j}] = {numbers[i,i]}");
+                for (int j = 0; j < 20; j++)
+                {
+                    if (numbers[i,j] < 10 )
+                    {
+                        numbers[i, j] = int.Parse(getNum());
+                        Console.WriteLine($"[{i:00} , {j:00}] = {numbers[i, j]:00}");
+                    }
+                }
             }
             Console.ReadKey();
-        }
+        }    
 
-        static string getNum(int N)
+        static string getNum()
         {
-            string num = "";
+            
+            int X = rnd.Next(0, 100 + 1);
+            string res = "";
 
-            if (N < 10)
+            if (X < 10)
             {
-                num = "0" + N.ToString();
+                res = X.ToString();
             }
             else
             {
-                num = N.ToString();
+                res = X.ToString();
             }
-            return num;
+            return res;
         }
     }
 }
