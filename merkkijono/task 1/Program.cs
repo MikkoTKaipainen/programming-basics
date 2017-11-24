fe9ea3b07e2ae8e6ded3b3e5386c2ef3115d1c48
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2017-11-24 10:58:20 +0200

    added string handling

---
+++ merkkijono/task 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace merkkijono
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Kirjoita lause ja kone kertoo monta merkkiä syötteessä on!");
+            Console.Write("Anna bala: ");
+            string userInput = Console.ReadLine();
+            for (int i = 0; i < userInput.Length; i++)
+            {
+            }
+            Console.Write($"Syötteessä on {userInput.Length} merkkiä");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ merkkijono/task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace merkkijono
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Kirjoita lause ja kone kääntää e kirjaimet euron merkeiksi!");
+            Console.Write("Anna bala: ");
+            string userInput = Console.ReadLine();
+            string userOutput = userInput.Replace("e", "€");
+            Console.Write($"{userOutput}");
+            Console.ReadKey();
+        }
+    }
+}
